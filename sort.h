abff069cfeee22dbae8f3639ff1f0d48e5e5d8ae
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2023-07-25 12:35:05 +0100

    Files of task 101 are created.

--- sort.h
+++ sort.h
@@ -35,5 +35,6 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
 
 #endif /* _SORT_H_ */
